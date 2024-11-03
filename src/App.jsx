51a8134410fa525dad5715cbc2624be25195f5fa
Muhammad Ramdhan Azizi <115683965+Azizi123haha@users.noreply.github.com>
2024-11-03 16:27:11 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import Header from './components/Header';
-import Pricing from './components/Pricing';
-import Features from './components/Features';
-import About from './components/About';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  background-color: #121212;
-  color: #f0f0f0;
-  min-height: 100vh;
-  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f0f0;
 `;
 
-function App() {
+const App = () => {
   return (
     <Container>
-      <Header />
-      <Pricing />
-      <Features />
-      <About />
+      <h1>Welcome to Az1zi Botz</h1>
     </Container>
   );
-}
+};
 
 export default App;
