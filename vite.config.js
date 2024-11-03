3dc77161c717c139d9829c460475cdc3d8636b4f
Muhammad Ramdhan Azizi <115683965+Azizi123haha@users.noreply.github.com>
2024-11-03 16:25:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Port default. Ubah jika diperlukan.
-  },
-  build: {
-    outDir: 'dist', // Folder output setelah build
-  },
 });
